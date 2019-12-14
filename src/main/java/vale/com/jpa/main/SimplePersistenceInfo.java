fa752d42ea718a6d99c651964823fcdf63a01cd3
Valeria Torres <torresvalemdz@gmail.com>
2019-12-14 12:26:36 -0300

    trabajando sin persistence xml simulando un contenedor de applicaciones

---
+++ src/main/java/vale/com/jpa/main/SimplePersistenceInfo.java
@@ -0,0 +1,136 @@
+package vale.com.jpa.main;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import javax.persistence.SharedCacheMode;
+import javax.persistence.ValidationMode;
+import javax.persistence.spi.ClassTransformer;
+import javax.persistence.spi.PersistenceUnitInfo;
+import javax.persistence.spi.PersistenceUnitTransactionType;
+import javax.sql.DataSource;
+
+public class SimplePersistenceInfo implements PersistenceUnitInfo {
+	
+	private final String name;
+	
+
+	public SimplePersistenceInfo(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String getPersistenceUnitName() {		
+		return name;
+	}
+
+	@Override
+	public String getPersistenceProviderClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PersistenceUnitTransactionType getTransactionType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataSource getJtaDataSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataSource getNonJtaDataSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getMappingFileNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<URL> getJarFileUrls() {
+		 
+		try {
+			return  Collections.list( this.getClass()
+			.getClassLoader()
+			.getResources("") );
+		} catch (IOException e) { 
+			throw new UncheckedIOException(e);
+		}
+		
+	}
+
+	@Override
+	public URL getPersistenceUnitRootUrl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getManagedClassNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean excludeUnlistedClasses() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public SharedCacheMode getSharedCacheMode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ValidationMode getValidationMode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Properties getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPersistenceXMLSchemaVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClassLoader getClassLoader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTransformer(ClassTransformer transformer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ClassLoader getNewTempClassLoader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
